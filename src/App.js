26a41974b92905d964517a51c5dac0029836326e
Aadarsh <aadarshjha6783@gmail.com>
2023-07-08 22:24:29 +0530

    Removed router for github pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-    Routes,
-  Route,
-} from "react-router-dom";
-
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -30,13 +24,13 @@ function App() {
       setMode('dark');
       setButtonState("checked");
       document.body.style.backgroundColor = '#2C3333';
-      showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode has been enabled", "dark");
     }
     else {
       setMode('light');
       setButtonState("");
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      showAlert("Light mode has been enabled", "danger");
     }
   }
   return (
@@ -44,10 +38,9 @@ function App() {
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} buttonState = {buttonState}/>
       <Alert alert={alert} />
       <div className="container my-3"></div>
-    <Routes>
-        <Route exact path="/about" element = {<About mode={mode} />}/>
-        <Route exact path="/" element= {<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />}/>
-    </Routes>
+    
+    <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
+
     </div>
   );
 }
